934bc30a4dddcec74c4799cc9e9fc07a77f5780e
Vitalij98 <vitali.mazzucchi2@gmail.com@gmail.com>
2022-09-09 12:53:11 +0200

    first commit

---
+++ src/main/java/com/epicode/managment/DeviceManagmentEmployeesApplication.java
@@ -0,0 +1,13 @@
+package com.epicode.managment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceManagmentEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceManagmentEmployeesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epicode/managment/DeviceManagmentEmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epicode.managment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceManagmentEmployeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
